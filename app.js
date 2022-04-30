43769cb2686ec0db757da89c4c6689b093d9c237
matidsc <dmatiutu211102@gmail.com>
2022-04-30 05:33:23 -0300

    Toggle menu terminado, faltan animaciones

---
+++ app.js
@@ -0,0 +1,9 @@
+function ToggleMenu() {
+    var x = document.getElementById("nav");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+
+}
