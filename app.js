f5d5bf65ed9c4f19eaac379c5fc8a2a319465935
matidsc <dmatiutu211102@gmail.com>
2022-05-02 00:10:43 -0300

    disabled scroll con menu abierto

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 function ToggleMenu() {
-  console.log('testing')
-    var x = document.getElementById("nav");
+
+  var x = document.getElementById("nav");
     if (x.style.display === "flex") {
       x.style.display = "none";
+      document.body.classList.remove('disabledScroll')
+
+
     } else {
       x.style.display = "flex";
+      document.body.classList.add('disabledScroll')
+
     }
 
 }
